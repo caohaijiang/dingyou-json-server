435968a9c2537d933321dfbbaea836516026cda6
曹海江 <caohj@xtding.cn>
2017-04-10 04:22:16 +0800

    fix

--- install.sh
+++ install.sh
@@ -2,3 +2,4 @@
 sudo cp ./myproj-jsonServer.service /lib/systemd/system/myproj-jsonServer.service
 sudo chmod 754 /lib/systemd/system/myproj-jsonServer.service
 sudo systemctl enable myproj-jsonServer
+sudo systemctl start myproj-jsonServer
